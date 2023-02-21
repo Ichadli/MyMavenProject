293f2e2c35d64d55c527a5d7fd9064e71cfedfb2
c.ismael <hazon.shop@gmail.com>
2023-02-20 19:56:57 -0500

    Added day-08 (Finished Example2)

--- src/test/java/technostudyB7/day8/FindElementsExample2.java
+++ src/test/java/technostudyB7/day8/FindElementsExample2.java
@@ -2,6 +2,7 @@ package technostudyB7.day8;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+
 import technostudyB7.Utilities.UtilityClass;
 
 import java.util.ArrayList;
@@ -9,7 +10,7 @@ import java.util.List;
 
 public class FindElementsExample2 extends UtilityClass {
     public static void main(String[] args) {
-        /**1- Go to https://www.saucedemo.com/
+        /*1- Go to https://www.saucedemo.com/
          2- login with valid credentials
          3- Add 3 random products to the cart
          4- Click on the cart
@@ -28,12 +29,14 @@ public class FindElementsExample2 extends UtilityClass {
         WebElement loginButton = driver.findElement(By.xpath("//input[@class='submit-button btn_action']"));
         loginButton.click();
 
-        List<WebElement> inventoryItems = driver.findElements(By.cssSelector(".inventory_item_name"));
+        List<WebElement> inventoryItems;
         List<String> inventoryItemsNames = new ArrayList<>();
-
         List<Integer> indexList = new ArrayList<>();
 
+
+
         while (inventoryItemsNames.size() < 3) {
+            inventoryItems = driver.findElements(By.cssSelector(".inventory_item_name"));
 
             int randomIndex = (int) (Math.random() * inventoryItems.size());
 
@@ -53,6 +56,29 @@ public class FindElementsExample2 extends UtilityClass {
 
         WebElement cartIcon = driver.findElement(By.className("shopping_cart_badge"));
         cartIcon.click();
+
+        WebElement checkOutButton = driver.findElement(By.id("checkout"));
+        checkOutButton.click();
+
+        WebElement nameInput = driver.findElement(By.id("first-name"));
+        nameInput.sendKeys("John");
+
+        WebElement lastnameInput = driver.findElement(By.id("last-name"));
+        lastnameInput.sendKeys("Snow");
+
+        WebElement zipCode = driver.findElement(By.id("postal-code"));
+        zipCode.sendKeys("11101");
+
+        WebElement continueButton=driver.findElement(By.id("continue"));
+        continueButton.click();
+
+        List<WebElement> checkOutItemList = driver.findElements(By.className("inventory_item_name"));
+
+        for (WebElement product : checkOutItemList) {
+            System.out.println(inventoryItemsNames.contains(product.getText()));
+        }
+
+        quiteDriver(20);
 
 
     }
